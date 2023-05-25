9a1c0e5fc163993bd2f85e85d40153a9696b030b
Carlos <carlosromiobezerra@gmail.com>
2023-05-25 19:06:19 -0300

    beta
    implementação de funções com parametros

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,5 @@
+import pygame
+
 def ler_linha_arquivo(nome_arquivo, numero_linha):
     with open(nome_arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
@@ -19,3 +21,22 @@ def salvar():
     arquivo2 = open("nome.txt", 'a+')
     arquivo2.write(arquivo)
     arquivo2.close()
+
+def linha(tela, branco, primeira2, segunda2):
+    pontolinha1 = ler_linha_arquivo("memoria.txt", primeira2)
+    coordenada1 = list(pontolinha1)
+    if coordenada1[3] == ",":
+        coordenada1[3] = None
+    coordenadax = coordenada1[1] + coordenada1[2] + coordenada1[3]
+    coordenaday = coordenada1[6] + coordenada1[7] + coordenada1[8]
+    coordenada1 = (int(coordenadax), int(coordenaday))
+    
+    pontolinha2 = ler_linha_arquivo("memoria.txt", segunda2)
+    coordenada2 = list(pontolinha2)
+    if coordenada2[3] == ",":
+        coordenadax = coordenada2[1] + coordenada2[2]
+    else:
+        coordenadax = coordenada2[1] + coordenada2[2] + coordenada2[3]
+    coordenaday = coordenada2[6] + coordenada2[7] + coordenada2[8]
+    coordenada2 = (int(coordenadax), int(coordenaday))
+    pygame.draw.line(tela, branco, (coordenada1), (coordenada2), 1)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import simpledialog
-import pygame
-from arquivo import ler_linha_arquivo, ler_arquivo, salvar
+import pygame 
+
+from arquivo import ler_linha_arquivo, ler_arquivo, salvar, linha
 pygame.init()
 pygame.font.init()
 
@@ -16,6 +17,9 @@ segunda = 1
 running = True
 true = False
 tela = pygame.display.set_mode(tamanho)
+pontos = 0
+primeira2 = 1
+segunda2 = 3
 
 pygame.display.set_caption('Space Marker')
 pygame.display.set_icon(icone)
@@ -26,17 +30,18 @@ tela.fill(branco)
 tela.blit( fundo, (0,0) )
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             salvar()
             ler_arquivo("memoria.txt")
             running = False
 
+
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             salvar()
             ler_arquivo("memoria.txt")
             running = False
+
 
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F2:
             ler_arquivo("nome.txt")
@@ -53,7 +58,6 @@ while running:
                     coordenadax = coordenada[1] + coordenada[2] + coordenada[3]
                     coordenaday = coordenada[6] + coordenada[7] + coordenada[8]
                     coordenada = (int(coordenadax), int(coordenaday))
-                    
                     texto = fontesys.render(arquivo + posicao, 1, branco)
                     tela.blit(texto, coordenada)
                     pygame.draw.circle(tela, branco, coordenada, 5)
@@ -61,27 +65,36 @@ while running:
                     segunda = segunda + 2
                 except:
                     true = False
+
                     
         elif event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            item = simpledialog.askstring('Space', 'Nome da estrela: ')
-            if item != '':
-                pos1 = item + "\n" + str(pos)
-                arquivo = open('memoria.txt', 'a+')
-                arquivo.write(pos1 + '\n')
-                arquivo.close()
-                texto = fontesys.render(pos1, 1, branco)
-                tela.blit(texto, pos)
+            try:
+                pos = pygame.mouse.get_pos()
+                item = simpledialog.askstring('Space', 'Nome da estrela: ')
+                if item != '':
+                    pos1 = item + "\n" + str(pos)
+                    arquivo = open('memoria.txt', 'a+')
+                    arquivo.write(pos1 + '\n')
+                    arquivo.close()
+                    texto = fontesys.render(pos1, 1, branco)
+                    tela.blit(texto, pos)
+                    pygame.draw.circle(tela, branco, pos, 5)
+                    pontos = pontos +1
+                    if pontos >= 2:
+                        linha(tela, branco, primeira2, segunda2)
+                        primeira2 = primeira2 + 2
+                        segunda2 = segunda2 + 2
 
-                pygame.draw.circle(tela, branco, pos, 5)
-                
-            else:
-                arquivo = open('memoria.txt', 'a+')
-                arquivo.write("Desconhecido" + '\n' + str(pos) + '\n')
-                arquivo.close()
-                texto = fontesys.render('Desconhecido'+ str(pos), 1, branco)
-                tela.blit(texto, pos)
-                pygame.draw.circle(tela, branco, pos, 5)
+                    
+                else:
+                    arquivo = open('memoria.txt', 'a+')
+                    arquivo.write("Desconhecido" + '\n' + str(pos) + '\n')
+                    arquivo.close()
+                    texto = fontesys.render('Desconhecido'+ str(pos), 1, branco)
+                    tela.blit(texto, pos)
+                    pygame.draw.circle(tela, branco, pos, 5)
+            except:
+                '''nada'''
 
 
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F3:
